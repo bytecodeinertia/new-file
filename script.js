cf875914fd9faa5563ace338fc973fbdd548c0fb
bytecodeinertia <caesardave744@gmail.com>
2023-02-10 14:42:40 +0100

    second commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function myFunction(){
+    document.getElementById("demo1").innerHTML="david"
+    document.getElementById("demo2").innerHTML="emeka"
+    document.getElementById("demo3").innerHTML="oweh";
+}
